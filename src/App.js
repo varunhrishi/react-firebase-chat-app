536f730b2396d156d6ad9dd36cda6458f0dc59c2
varunhrishi <70848751+varunhrishi@users.noreply.github.com>
2021-10-22 01:11:34 +0530

    add config

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  // your config
+  // your config 
 })
 
 const auth = firebase.auth();
